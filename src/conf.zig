2ecc4da6807eed0712d99ee1474cc77b05173e22
Dorian <dorian@resalocal.com>
2023-04-02 01:23:44 +0200

    adjust ocnf

--- src/conf.zig
+++ src/conf.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub const CELL_SIZE = 30;
 
 pub const SCREEN_WIDTH = 800;
-pub const SCREEN_HEIGHT = 1000;
+pub const SCREEN_HEIGHT = 1200;
 
 pub const MAX_HEIGHT = @divTrunc(SCREEN_HEIGHT, CELL_SIZE);
 pub const MAX_WIDTH = @divTrunc(SCREEN_WIDTH, CELL_SIZE);
