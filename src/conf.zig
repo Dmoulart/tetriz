59c7d3a128af6d02409bb7ac100c47d219e5c450
Dorian <dorian@resalocal.com>
2023-04-02 01:40:30 +0200

    update conf

--- src/conf.zig
+++ src/conf.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
 
-pub const CELL_SIZE = 30;
+pub const CELL_SIZE = 30; // 5 lol
 
 pub const SCREEN_WIDTH = 800;
-pub const SCREEN_HEIGHT = 1200;
+pub const SCREEN_HEIGHT = 1000;
 
 pub const MAX_HEIGHT = @divTrunc(SCREEN_HEIGHT, CELL_SIZE);
 pub const MAX_WIDTH = @divTrunc(SCREEN_WIDTH, CELL_SIZE);
